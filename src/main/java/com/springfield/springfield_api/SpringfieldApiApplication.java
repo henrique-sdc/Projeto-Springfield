a97d3bd999c38d728105b9cb2b18796199eaa965
Henrique <henrique.scastro12@gmail.com>
2025-03-01 16:31:46 -0300

    Base do Projeto
    Spring Initializr

---
+++ src/main/java/com/springfield/springfield_api/SpringfieldApiApplication.java
@@ -0,0 +1,13 @@
+package com.springfield.springfield_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfieldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfieldApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springfield/springfield_api/SpringfieldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springfield.springfield_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfieldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
